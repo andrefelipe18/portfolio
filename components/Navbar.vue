dc0c5085695ddc57fb22a11fb4e53bc70e1cf1f1
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-10-06 00:03:56 -0300

    wip

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,7 +7,6 @@ defineProps<{
 	<nav class="flex items-center justify-center p-6 mx-auto lg:px-8">
 		<ul class="flex flex-wrap items-center justify-center gap-x-6 sm:gap-x-12">
 			<NavItem to="/" text="Sobre" :path="path" />
-			<NavItem to="/projects" text="Projetos" :path="path" />
 			<NavItem to="/contact" text="Contato" :path="path" />
 			<NavItem to="/resume" text="Currículo" :path="path" />
 			<ResumeDownloadButton v-if="path === '/resume'" />
